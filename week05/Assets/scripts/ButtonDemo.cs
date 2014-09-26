5020e9d88be8eec75cb595a3ddfb6d2e010ca2d2
Robert Yang <yang.robert.w@gmail.com>
2014-09-26 15:11:25 -0400

    added week 5 (physics code stuff, making simple buttons)

---
+++ week05/Assets/scripts/ButtonDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonDemo : MonoBehaviour {
+
+	void OnMouseUpAsButton () {
+		// whatever code I put inside here will HAPPEN only when:
+		// - the object has a collider
+		// - and the user hovered their mouse cursor over this collider
+		// - and the user clicked their LEFT mouse button
+		// - and the user released their LEFT mouse button
+		// - and the user STILL has their mouse cursor over this collider
+		
+		// increase in size by 10%, using the *= operator
+		transform.localScale *= 1.1f; 
+		// the code above is the same as typing this:
+		// transform.localScale = transform.localScale * 1.1f;
+	}
+	
+}
---
+++ week05/Assets/scripts/CubeDemo.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeDemo : MonoBehaviour {
+
+	// PUBLIC does:
+	// 1) exposes it in the inspector
+	// 2) lets other C# scripts access this
+	public int babies = 699991;
+	
+	// we can also make other variables "public"
+	public Color lightColor;
+	
+	// we can make "public" references to other objects
+	// Unity asks us "which light in the scene are you talking about?"
+	public Light whichLight; // note to self: assign in Inspector
+	
+	// "which rigidbody are you talking about? this one or that one?"
+	public Rigidbody kanye; // note to self: assign in Inspector
+	
+
+	void Update () {
+		// every frame, update the light's color with the color in our public variable
+		whichLight.color = lightColor;
+		
+		// Input.GetKeyDown = you must tap, Input.GetKey = you can hold it down
+		if ( Input.GetKey (KeyCode.Space ) ) {
+			kanye.AddForce ( new Vector3( 0f, 50f, 0f ) ); // Y axis vector = up
+		}
+		
+		// GetAxis(Horizontal) == -1 if user presses LeftArrow, +1 if user presses RightArrow
+		kanye.AddForce ( new Vector3(50f, 0f, 0f) * Input.GetAxis ( "Horizontal" ) );
+		// GetAxis(MouseX) == -1 if user moves mouse left, +1 if user moves mouse right
+		kanye.AddTorque ( new Vector3(0f, 10f, 0f) * Input.GetAxis ( "Mouse X") );
+		
+		// this might only work ONCE, because deactivating this object
+		// might actually deactivate the code (THIS code) on this object too!
+		if ( Input.GetKeyDown (KeyCode.K) ) {
+			kanye.gameObject.SetActive ( !kanye.gameObject.activeInHierarchy );
+		}
+		
+	}
+}
