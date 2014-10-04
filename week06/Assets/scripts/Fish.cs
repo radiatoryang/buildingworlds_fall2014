491df14efa8cf77854f8d8e536f5469ed53c3776
Robert Yang <yang.robert.w@gmail.com>
2014-10-04 12:03:59 -0400

    added week 6: intro to vector math

---
+++ week06/Assets/scripts/Fish.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 1) we made the fish go to a destination
+// 2) we debugged that behavior, added a distance check
+// 3) we also made it come to a full stop upon failing the distance check
+// 4) we also normalized the fish's force vector, consistent move speed
+// 5) we plugged the food pellet position into the "destination"
+
+public class Fish : MonoBehaviour {
+
+	public Vector3 destination;
+	public float speed = 0.2f;
+	
+	public Transform foodPellet; // assign in inspector
+	
+	// Update is called once per frame
+	void Update () {
+	
+		destination = foodPellet.position;
+	
+		// is the fish within 5 units of its destination? then stop swimming
+		if ( Vector3.Distance(destination, transform.position) > 1f) {
+			rigidbody.AddForce ( Vector3.Normalize(destination - transform.position) * speed );
+		} else {
+			rigidbody.velocity = Vector3.zero; // come to complete stop
+		}
+		
+	} // update
+} // fish class
