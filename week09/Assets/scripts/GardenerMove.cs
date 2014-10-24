c5aa15ee5d43eee675c91ce89b623c63c4c44046
Robert Yang <yang.robert.w@gmail.com>
2014-10-24 16:37:41 -0400

    added week 9 (using raycast for a gardening simulation)

---
+++ week09/Assets/scripts/GardenerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GardenerMove : MonoBehaviour {
+
+	public float moveSpeed = 500f; // movement speed tuning variable
+
+	// Update is called once per frame
+	void Update () {
+		// apply a physics force to this object's rigidbody to make it "walk"
+		rigidbody.AddForce ( transform.forward * Time.deltaTime * moveSpeed );
+
+		// shoot a raycast 4 meters in front of me; if it hits something, then
+		// I should randomly turn left or right
+		if (Physics.Raycast ( transform.position, transform.forward, 4f ) ) {
+			// Mathf.Sign returns -1 or 1 based on whether the number is negative or not
+			transform.Rotate (0f, Mathf.Sign ( Random.Range (-1f, 1f) ) * 90f, 0f );
+		}
+
+		// visualize raycast in debug mode (scene view)
+		Debug.DrawRay ( transform.position, transform.forward * 4f, Color.cyan );
+	}
+}
---
+++ week09/Assets/scripts/GardenerPlantTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GardenerPlantTime : MonoBehaviour {
+
+	public Transform treePrefab; // assign in inspector
+	public Transform gardenerPrefab; // assign in inspector
+
+	float nextPlantingTime = 0f; // the time, in seconds, when I should plant again
+
+	void Start () {
+		nextPlantingTime = Time.time + 5f; // when I am born, set my next plant time later
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// if it is time to plant, then...
+		if (Time.time > nextPlantingTime ) {
+			if (Random.Range (0f, 1f) > 0.1f ) { // 90% chance of planting a tree
+				Instantiate ( treePrefab, transform.position, Quaternion.identity );
+			} else { // 10% chance of planting another gardener
+				Instantiate ( gardenerPrefab, transform.position - transform.forward, Quaternion.identity );
+			}
+			nextPlantingTime += Random.Range( 5f, 10f); // set the next planting time
+		}	
+	}
+}
